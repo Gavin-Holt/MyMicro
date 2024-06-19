2bbdf8ee9b7e66e21a43473c6e3e0efb4144afb6
Gavin Holt <holtgavin@gmail.com>
2024-06-19 20:45:26 +0000

    Add files via upload

--- micro.bat
+++ micro.bat
@@ -5,7 +5,11 @@ REM Set Lua path to use pure Lua libraries
 set LUA_PATH=;;O:\MyProfile\lua\lua\?.lua;O:\MyProfile\lua\lua\?\init.lua         >nul 2>nul
 
 REM Launch micro.ahk
-start micro.ahk
+start autohotkey.exe micro.ahk
+
+REM Make VERSION.txt
+micro.exe -version > VERSION.txt
+ver >> VERSION.txt
 
 REM Launch Micro with a title "Micro.exe" and use local config directory
 start  "micro.exe" /MAX  %~dp0micro.exe --config-dir %~dp0confMicro %*
